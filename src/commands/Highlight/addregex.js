6504b06b11bfb7b043979d64e3870fd76c650b71
Unknown <jake@gealer.email>
2018-06-28 10:23:05 +0100

    Added addregex.js

---
+++ src/commands/Highlight/addregex.js
@@ -0,0 +1,31 @@
+const { Command } = require("klasa");
+
+module.exports = class extends Command {
+    constructor (...args) {
+        super(...args, {
+            runIn: ["text"],
+            description: "Adds a regex string to your highlight list",
+            usage: "<Word:str>",
+        });
+        this.needsMember = true;
+    }
+
+    async run (msg, [regex]) {
+        if (msg.member.configs.regex_queries.includes(regex)) {
+            return msg.send(null, {
+                embed: {
+                    color: 0xCC0F16,
+                    description: `You already have that regex in your regex list!`,
+                },
+            });
+        }
+        await msg.member.configs.update("regex_queries", regex);
+        msg.guild.addCachedRegex(regex, msg.member);
+        return msg.send(null, {
+            embed: {
+                color: 0x43B581,
+                description: `Done! \`${regex}\` has been added to your highlight list.`,
+            },
+        });
+    }
+};
