7f03fe67cb0bee89a5e900cd1c35b6b0cec05c59
Unknown <jake@gealer.email>
2018-06-28 10:27:56 +0100

    Adjusted clear.js

--- src/commands/Highlight/clear.js
+++ src/commands/Highlight/clear.js
@@ -5,28 +5,35 @@ module.exports = class extends Command {
 		super(...args, {
 			runIn: ["text"],
 			cooldown: 5,
-			description: "Clears your highlighted word list",
+			description: "Clears your highlighted word/reged list",
 		});
 		this.needsMember = true;
 	}
 
 	async run (msg) {
-		if (!msg.member.configs.words.length) {
+		if (!msg.member.configs.words.length || !msg.member.configs.regex_queries.length) {
 			return msg.send(null, {
 				embed: {
 					color: 0xCC0F16,
-					description: `You don't have any words to clear!`,
+					description: `You don't have any words/regex to clear!`,
 				},
 			});
 		}
-		for (const word of msg.member.configs.words) {
-			msg.guild.removeCachedWord(word, msg.member);
-		}
+		if (msg.member.configs.words.length) {
+            for (const word of msg.member.configs.words) {
+                msg.guild.removeCachedWord(word, msg.member);
+            }
+        }
+        if (msg.member.configs.regex_queries.length) {
+            for (const r of msg.member.configs.regex_queries) {
+                msg.guild.removeCachedRegex(r, msg.member);
+            }
+        }
 		await msg.member.configs.reset("words", msg.guild);
 		return msg.send(null, {
 			embed: {
 				color: 0x43B581,
-				description: `Done! Your highlight word list has been cleared!`,
+				description: `Done! Your highlight list has been cleared!`,
 			},
 		});
 	}
